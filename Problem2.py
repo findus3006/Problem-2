9900ff291088f341160dc67c55654813beab7e3f
unknown <rolfs.stefan@gmail.com>
2023-02-08 18:34:31 +0100

    Problem 2
    Redone with Jupyter

--- Problem2.py
+++ Problem2.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib as plt
 
+#Problem a: Phasefactor matrix
+
 def P(ni,Di,k0):
     Phasetransfer = np.zeros((2,2),dtype = 'complex');
     Phasetransfer[0,0] = np.exp(1j*k0*Di*ni)
     Phasetransfer[1,1] = np.exp(-1j*k0*Di*ni)
     return Phasetransfer
 
-A = np.zeros((2,2),dtype = 'complex');
-A[0,0] = np.exp(1j*1);
 print(P(1,1,1))
 
